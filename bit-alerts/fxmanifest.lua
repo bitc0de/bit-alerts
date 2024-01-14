6f59db005673c36b7f4595ad46a23b120ba23a0d
bitc0de <keyvenm@gmail.com>
2024-01-14 12:30:30 +0100

    Update fxmanifest.lua

--- bit-alerts/fxmanifest.lua
+++ bit-alerts/fxmanifest.lua
@@ -5,4 +5,4 @@ client_scripts {'config/shared.lua', 'config/c_config.lua', 'client/main.lua'}
 
 ui_page "html/index.html"
 
-files {'html/index.html', 'html/script.js', 'html/style.css', 'html/assets/*'}
+files {'html/index.html', 'html/script.js', 'html/assets/*'}
